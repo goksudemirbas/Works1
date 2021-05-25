cb39002eaabd3209debdd3ecc29b35d52ac6b6d4
Goksu <goksu_demirbas@hotmail.com>
2021-05-25 14:57:49 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    class EFProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var x = context.Entry(entity);
+                x.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var y = context.Entry(entity);
+                y.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }  
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList();
+            }
+
+         }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var z = context.Entry(entity);
+                z.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
